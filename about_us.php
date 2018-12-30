797afd1b48097c5641c202c20986b80af50b19db
qaisark <qaisarkasana6@gmail.com>
2018-12-30 12:42:44 +0500

    admin dashboard fixed 3

---
+++ about_us.php
@@ -0,0 +1,28 @@
+<?php include 'includes/config.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	    <title>Hostel Tracker</title>
+    <meta charset="utf-8">
+    <?php include 'includes/links.php'; ?>
+</head>
+<body>
+<?php
+    include "includes/header.php";
+?> 
+ <!--   Main Content  -->
+ <div>
+ <div id="carea">
+	<h1>Quick Contact Us!</h1>
+	<p>Get in touch with us to find all your quiries</p>
+	<div id="about-section">
+	
+	</div>
+</div>
+ </div>
+  <?php 
+    include "includes/footer.php";
+    ?>
+</body>
+</html>
+
