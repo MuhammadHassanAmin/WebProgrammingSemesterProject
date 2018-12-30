20a83d9da28ae07866d69d2da37c822294b53abd
qaisark <qaisarkasana6@gmail.com>
2018-12-30 14:56:05 +0500

    about us done

--- about_us.php
+++ about_us.php
@@ -11,21 +11,46 @@
     include "includes/header.php";
 ?> 
  <!--   Main Content  -->
- <div>
- <div id="carea">
-	<h1>About Us!</h1>
+ <div id="about-banner">
+        <div id="banner-text">
+        	<h1>About Hostels Hub</h1>
+        </div>
+</div>
+ <div id="abt">
 	<div id="about-section">
-        HostelHub, the world's largest travel site*, enables travelers to unleash the full potential of every trip. 
+       <p> HostelHub, the world's largest travel site, enables travelers to unleash the full potential of every trip. 
         With 702 million reviews and opinions covering the world's largest selection of travel listings worldwide – 
         covering 8 million accommodations, airlines, experiences, and restaurants -- HostelHub provides travelers with 
         the wisdom of the crowds to help them decide where to stay, how to fly, what to do and where to eat. 
         HostelHub also compares prices from more than 200 hotel booking sites so travelers can find the lowest price 
         on the hotel that's right for them. HostelHub-branded sites are available in 49 markets, and are home to the 
-        world's largest travel community of 490 million average monthly unique visitors**, all looking to get the 
-        most out of every trip. HostelHub: Know better. Book better. Go better.
+        world's largest travel community of 490 million average monthly unique visitors, all looking to get the 
+        most out of every trip. HostelHub: Know better. Book better. Go better.</p>
+        <p>
+        Hostels.com is the ultimate resource for great value accommodation all over the world. Our team works with savvy travellers worldwide to continually offer you great new deals on the fastest-growing list of hostels, B&B's, budget hotels, and other accommodation online. The result? We offer the most comprehensive selection on the internet with over 33,000 great places to stay in 170 countries!
+        </p>     
+        <p>
+        Hostels.com also offers in-depth advice to budget and independent travellers, including city guides, videos & podcasts, travel features, newsletters and FAQs on hostelling and backpacking.
+        </p>
 	</div>
-</div>
- </div>
+    <div class="add">
+		<div class="entry-content">
+            <h2>Mailing Address:</h2>
+            <p>*Please notify us (email us using the form below or call: 051-1234567) before you send anything to the us. </p>
+            <h3>For United State Postal Service (USPS), Canada Post or other national postal services mailing address</h3>
+            <p>Hostel Tahoe<br />
+            PO Box 577<br />
+            Kings Beach, CA 96143-0577<br />
+            USA</p>
+            <h3>For FedEx, DHL, UPS, or other courier shipments</h3>
+            <p>Your name (c/o Hostel Tahoe)<br />
+            8931 N. Lake Blvd<br />
+            Box 577<br />
+            Kings Beach, CA 96143-0577<br />
+            USA</p>
+        </div>
+    </div>
+    </div>
   <?php 
     include "includes/footer.php";
     ?>
