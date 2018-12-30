b9d715eae44a6e744923a79290f0fb5799670886
HassanAmin <hasan_amin3@yahoo.com>
2018-12-30 20:31:38 +0500

    teams & policy page fixed. -bg changed -style fixed -bg position on sm screen fixed

--- includes/contact_form.php
+++ includes/contact_form.php
@@ -5,33 +5,38 @@
 		<div class="sep"></div>
 		<br>
 		<form method="POST" action="<?php echo $path ?>send_contact.php">
-    		 <div id="name_input">
-            	<?php 
+			<div id="name_input">
+				<?php 
 				if (isset($_COOKIE['FullName'])) {?>
-			     <input name="name" placeholder="Name" required="required" type="text" value="<?php echo $_COOKIE['FullName'] ?>" pattern="[A-Za-z\s]+{30}"><?php	
+				<input name="name" placeholder="Name" required="required" type="text" value="<?php echo $_COOKIE['FullName'] ?>"
+				 pattern="[A-Za-z\s]+{30}">
+				<?php	
     			}
     			else{
             	?>
-                <input name="name" placeholder="Name" required="required" type="text" pattern="[A-Za-z\s]+{30}"><?php } ?>
-            </div>
-            <div id="email_input">
-            	<?php 
+				<input name="name" placeholder="Name" required="required" type="text" pattern="[A-Za-z\s]+{30}">
+				<?php } ?>
+			</div>
+			<div id="email_input">
+				<?php 
 				if (isset($_COOKIE['Email'])) {?>
-			     <input name="email"  required="required" type="text" value="<?php echo $_COOKIE['Email'] ?>"><?php	
+				<input name="email" required="required" type="text" value="<?php echo $_COOKIE['Email'] ?>">
+				<?php	
     			}
     			else{
             	?>
-                <input name="email" placeholder="Email" required="required" type="email"><?php } ?>
-            </div>
-            <div id="ph_input">
-                <input name="sub" placeholder="Subject" required="required" type="text">
-            </div>
-    		<div id="msg_input">
-                <textarea name="msg" placeholder="your query..." rows="7" required="required"></textarea>
-            </div>
-    		<div id="cbtn">
-                <button type="submit" name="submit">Send</button>
-            </div>
+				<input name="email" placeholder="Email" required="required" type="email">
+				<?php } ?>
+			</div>
+			<div id="ph_input">
+				<input name="sub" placeholder="Subject" required="required" type="text">
+			</div>
+			<div id="msg_input">
+				<textarea name="msg" placeholder="your query..." rows="7" required="required"></textarea>
+			</div>
+			<div id="cbtn">
+				<button type="submit" name="submit">Send</button>
+			</div>
 		</form>
 		<br>
 	</div>
