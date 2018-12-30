68e85e4addd7b2a8e4f5428c204a0c92189f0ebf
qaisark <qaisarkasana6@gmail.com>
2018-12-30 12:55:14 +0500

    about work and gallary added

--- about_us.php
+++ about_us.php
@@ -13,10 +13,16 @@
  <!--   Main Content  -->
  <div>
  <div id="carea">
-	<h1>Quick Contact Us!</h1>
-	<p>Get in touch with us to find all your quiries</p>
+	<h1>About Us!</h1>
 	<div id="about-section">
-	
+        HostelHub, the world's largest travel site*, enables travelers to unleash the full potential of every trip. 
+        With 702 million reviews and opinions covering the world's largest selection of travel listings worldwide – 
+        covering 8 million accommodations, airlines, experiences, and restaurants -- HostelHub provides travelers with 
+        the wisdom of the crowds to help them decide where to stay, how to fly, what to do and where to eat. 
+        HostelHub also compares prices from more than 200 hotel booking sites so travelers can find the lowest price 
+        on the hotel that's right for them. HostelHub-branded sites are available in 49 markets, and are home to the 
+        world's largest travel community of 490 million average monthly unique visitors**, all looking to get the 
+        most out of every trip. HostelHub: Know better. Book better. Go better.
 	</div>
 </div>
  </div>
--- add_gallery.php
+++ add_gallery.php
@@ -9,9 +9,6 @@
     <?php include 'includes/links.php'; ?>
 </head>
 <body>
-    <?php
-        include "includes/header.php";
-    ?>
     <div class="admin-form">
     <?php 
             if (isset($_SESSION['email'])) {
