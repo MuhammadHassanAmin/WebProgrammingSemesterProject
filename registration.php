f571608f841d6f5e1ba797c55ec0338ff7a3eb51
HassanAmin <hasan_amin3@yahoo.com>
2018-12-30 11:23:48 +0500

    Registration Page Presentation layer Completed

--- registration.php
+++ registration.php
@@ -1,42 +1,52 @@
 <?php include 'includes/config.php'; ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hostel Tracker</title>
     <meta charset="utf-8">
     <?php include 'includes/links.php'; ?>
 </head>
+
 <body>
     <?php
         include "includes/header.php";
     ?>
-    <div class="admin-form">
+    <div id="carea">
         <h1>Hostel Owner Registration!</h1>
-        <form action="registration-action.php" method="post" enctype="multipart/form-data"> 
-            <div>
-                <input type="text" name="username" placeholder="user name" required>
-            </div>
-            <div>
-                <input type="text" name="email" placeholder="email" required>
-            </div>
-            
-            <div>
-                <input type="text" name="ph" placeholder="Phone Number" required>
-            </div>
-            <div>
-                <input type="text" name="city" placeholder="City" required>
-            </div>
-            <div>
-                <textarea name="address" placeholder="enter your address.." required></textarea>
-            </div>
-            <div>
-            <label for="">Select Profile Picture</label>
-                <input type="file" name="profile">
-            </div>
-            <div>
-                <button type="submit" name="registration_action">Register</button>
-            </div>
-        </form> 
+        <div id="fields">
+        <div class="sep"></div>
+		<br>
+
+                <form action="registration-action.php" method="post" enctype="multipart/form-data">
+                         <div id="name_input">
+
+                        <input type="text" name="username" placeholder="user name" required>
+                    </div>
+                    <div id="name_input">
+                        <input type="text" name="email" placeholder="email" required>
+                    </div>
+
+                    <div id="name_input">
+                        <input type="text" name="ph" placeholder="Phone Number" required>
+                    </div>
+                    <div id="name_input">
+                        <input type="text" name="city" placeholder="City" required>
+                    </div>
+                    <div id="name_input">
+                        <input type="text" name="address" placeholder="enter your address.." required>
+                    </div>
+                    <div class="mx-auto text-center"> 
+                        <label for="">Select Profile Picture</label><br>
+                        <input type="file" class="left-30" name="profile">
+                    </div>
+                    <div>
+                        <button type="submit" name="registration_action">Register</button>
+                    </div>
+                </form>
+           
+        </div>
     </div>
 </body>
+
 </html>
